4d84e3ffc8484d28ae221166c7d73c781311b568
raulhechavarria <raulhperez@gmail.com>
2026-08-24 19:38:56 -0400

    Translate architecture page label

--- src/app/pages/architecture/architecture.ts
+++ src/app/pages/architecture/architecture.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { LanguageService } from '../../services/language.service';
+
 @Component({
   selector: 'app-architecture',
   imports: [],
   templateUrl: './architecture.html',
   styleUrl: './architecture.css',
 })
-export class Architecture {}
+export class Architecture {
+  constructor(protected language: LanguageService) {}
+}
